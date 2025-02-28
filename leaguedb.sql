4511c42b144dfbb8c0e95ae7bb18ca0626cd8422
isshcrewyy <163158299+isshcrewyy@users.noreply.github.com>
2025-02-28 10:46:01 +0545

    sql

--- leaguedb.sql
+++ leaguedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2024 at 06:25 AM
+-- Generation Time: Feb 28, 2025 at 05:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -41,7 +41,10 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`admin_id`, `name`, `email`, `password`, `created_at`) VALUES
 (3, 'Diwas Shrestha', 'sthadiwas106@gmail.com', '$2y$10$mGk4crnuz9jSAPL00w8xr.mrFde86LUQYVG0bFxZXpElB9TZpZiiO', '2024-12-08 13:34:27'),
-(4, 'admin', 'admin@gmail.com', '$2y$10$TGVihFud1o3Xuvtj6eHltuBVR5DfiPC0E/ksffL7p4X/5NPxyjudy', '2024-12-09 16:19:36');
+(4, 'admin', 'admin@gmail.com', '$2y$10$TGVihFud1o3Xuvtj6eHltuBVR5DfiPC0E/ksffL7p4X/5NPxyjudy', '2024-12-09 16:19:36'),
+(5, 'JMC', 'diwas@gmail.com', '$2y$10$/GmQnx2sqEBHqZSrUMM.ReNl2fE9hFPAcXfGSgs5RdCSVee8y5X7S', '2025-01-15 04:18:45'),
+(6, 'carlo', 'shrestha@gmail.com', '$2y$10$hBCKX/jiAt9jzdv.oUQF0eL1BjyHryACYuGr.S7MMvnFHvSDfASKS', '2025-01-15 04:20:01'),
+(7, 'diwas', 'hhh@gmail.com', '$2y$10$PP36NW8OWLaDNXmX2WFn0.5c9heP8fOUgY8AQFk.AQtXcpDfnKJYi', '2025-01-15 04:37:55');
 
 -- --------------------------------------------------------
 
@@ -54,7 +57,7 @@ CREATE TABLE `club` (
   `c_name` varchar(100) NOT NULL,
   `league_id` int(11) DEFAULT NULL,
   `location` varchar(100) DEFAULT NULL,
-  `created_by` varchar(255) NOT NULL
+  `created_by` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -62,13 +65,17 @@ CREATE TABLE `club` (
 --
 
 INSERT INTO `club` (`club_id`, `c_name`, `league_id`, `location`, `created_by`) VALUES
-(49, 'BARCA', 43, 'spain', 'Diwas Shrestha'),
-(51, 'Real', 43, 'madrid', 'Diwas Shrestha'),
-(52, 'inter', 43, 'milano', 'Diwas Shrestha'),
-(53, 'city', 43, 'city', 'Diwas Shrestha'),
-(56, 'Kathmandu rayzers', 42, 'Kathmandu', 'org'),
-(57, 'Lalitpur pirates', 42, 'Lalitpur', 'org'),
-(58, 'dhangadhi fc', 42, 'dhangadhi', 'org');
+(99, 'fc manang ', 46, 'ktm', 67),
+(101, 'manag', 46, 'ktm', 67),
+(108, 'Kathmandu rayzers', 50, 'Kathmandu', 72),
+(109, 'Lalitpur pirates', 50, 'Lalitpur', 72),
+(110, 'Dhangadhi fc', 50, 'Dhangadhi', 72),
+(111, 'F.C. Chitwan', 50, 'Chitwan', 72),
+(112, 'Pokhara Thunders', 50, 'Pokhara', 72),
+(114, 'Butwal Lumbini FC', 50, 'Butwal', 72),
+(115, 'Birgunj United FC', 50, 'Birgunj', 72),
+(116, 'Sporting Illam', 50, 'Illam', 72),
+(117, 'Jhaapa FC', 50, 'Jhapa', 72);
 
 -- --------------------------------------------------------
 
@@ -91,7 +98,10 @@ CREATE TABLE `coach` (
 --
 
 INSERT INTO `coach` (`coach_id`, `co_name`, `experience`, `age`, `club_id`, `phone_number`, `created_by`) VALUES
-(17, 'carlo', 5, 70, 56, '3457483', 'org');
+(22, 'Rajendra Tamang', 6, 56, 117, '982343243', 'org'),
+(23, 'Binod Dangol', 6, 66, 116, '3432324', 'org'),
+(24, 'Prabin Shrestha', 6, 66, 108, '0', 'org'),
+(25, 'Suraj Bhusal', 3, 30, 112, '98766756', 'org');
 
 -- --------------------------------------------------------
 
@@ -121,9 +131,8 @@ CREATE TABLE `form` (
 --
 
 INSERT INTO `form` (`form_id`, `userId`, `duration`, `max_teams`, `one_league`, `start_date`, `end_date`, `experience`, `active`, `location`, `rules`, `status`, `created_at`, `league_name`) VALUES
-(16, 61, 1, 9, '', '2024-12-01', '2025-01-01', '5 years', 'yes', 'Kathmandu', 'lots of rules', 'approved', '2024-12-26 16:48:24', 'Nepal Super League'),
-(17, 62, 1, 1, '', '2024-12-02', '2025-01-02', '5 leagues', 'yes', 'kuleshwar', 'yes', 'approved', '2024-12-27 07:07:02', 'Janamaitri BCA CUP'),
-(18, 63, 1, 1, '', '2024-01-02', '2024-02-02', 'w', 'yes', 'Kathmandu', 'w', 'approved', '2024-12-27 08:40:06', 'Laliga');
+(20, 67, 1, 1, '', '2080-01-02', '2080-02-02', 'y', 'yes', 'Kathmandu', 'y', 'approved', '2025-02-23 08:13:50', 'Leaguepro'),
+(26, 72, 1, 4, 'yes', '0000-00-00', '2025-03-28', 'yes', 'yes', 'Kathmandu', 'Yes', 'approved', '2025-02-26 10:03:08', 'Nepal Super League');
 
 -- --------------------------------------------------------
 
@@ -148,8 +157,9 @@ CREATE TABLE `game` (
 --
 
 INSERT INTO `game` (`match_id`, `league_id`, `home_club_id`, `away_club_id`, `date`, `time`, `score_home`, `score_away`, `created_by`) VALUES
-(32, 42, 56, 57, '2024-12-02', '02:02:00', 6, 0, 'org'),
-(33, 43, 49, 51, '2024-12-04', '01:02:00', 9, 0, 'Diwas Shrestha');
+(68, 46, 99, 101, '2045-02-20', '01:02:00', 7, 0, '67'),
+(74, 50, 109, 108, '2025-03-09', '01:02:00', 1, 2, '72'),
+(75, 50, 117, 110, '2025-03-02', '01:02:00', 1, 0, '72');
 
 -- --------------------------------------------------------
 
@@ -177,10 +187,20 @@ CREATE TABLE `leaderboard` (
 --
 
 INSERT INTO `leaderboard` (`leaderboard_id`, `club_id`, `league_id`, `matches_played`, `wins`, `losses`, `draws`, `goals_scored`, `goals_against`, `goal_difference`, `points`, `created_by`) VALUES
-(21, 56, 42, 1, 1, 0, 0, 6, 0, 6, 3, 'org'),
-(22, 57, 42, 1, 0, 1, 0, 0, 0, -6, 0, 'org'),
-(23, 49, 43, 1, 1, 0, 0, 9, 0, 9, 3, 'Diwas Shrestha'),
-(24, 51, 43, 1, 0, 1, 0, 0, 0, -9, 0, 'Diwas Shrestha');
+(40, 102, 46, 1, 1, 0, 0, 9, 0, 7, 3, 'Diwas Shrestha'),
+(41, 99, 46, 1, 1, 0, 0, 7, 0, 7, 3, 'Diwas Shrestha'),
+(42, 105, 46, 4, 3, 1, 0, 18, 0, 12, 9, 'Diwas Shrestha'),
+(43, 107, 46, 1, 0, 1, 0, 0, 0, -9, 0, 'Diwas Shrestha'),
+(44, 106, 46, 1, 1, 0, 0, 9, 0, 9, 3, 'Diwas Shrestha'),
+(45, 101, 46, 1, 0, 1, 0, 0, 0, -7, 0, 'Diwas Shrestha'),
+(46, 108, 50, 1, 1, 0, 0, 2, 0, 1, 3, 'org'),
+(47, 109, 50, 1, 0, 1, 0, 1, 0, -1, 0, 'org'),
+(48, 111, 50, 1, 0, 0, 1, 0, 0, 0, 1, 'org'),
+(49, 112, 50, 1, 0, 0, 1, 0, 0, 0, 1, 'org'),
+(50, 117, 50, 1, 1, 0, 0, 1, 0, 1, 3, 'org'),
+(51, 110, 50, 1, 0, 1, 0, 0, 0, -1, 0, 'org'),
+(52, 114, 50, 1, 1, 0, 0, 7, 0, 7, 3, 'org'),
+(53, 116, 50, 1, 0, 1, 0, 0, 0, -7, 0, 'org');
 
 -- --------------------------------------------------------
 
@@ -194,7 +214,6 @@ CREATE TABLE `league` (
   `userId` int(11) NOT NULL,
   `start_date` int(11) DEFAULT NULL,
   `end_date` int(11) DEFAULT NULL,
-  `season` int(100) DEFAULT NULL,
   `status` varchar(20) DEFAULT 'pending',
   `duration` int(11) NOT NULL,
   `max_teams` int(11) NOT NULL,
@@ -205,10 +224,9 @@ CREATE TABLE `league` (
 -- Dumping data for table `league`
 --
 
-INSERT INTO `league` (`league_id`, `league_name`, `userId`, `start_date`, `end_date`, `season`, `status`, `duration`, `max_teams`, `location`) VALUES
-(42, 'Nepal Super League 2', 61, 2024, 2025, 4, 'pending', 1, 9, NULL),
-(43, 'Janamaitri BCA CUP', 62, 2024, 2025, NULL, 'pending', 1, 1, 'kuleshwar'),
-(44, 'Laliga', 63, 2024, 2024, NULL, 'pending', 1, 1, 'Kathmandu');
+INSERT INTO `league` (`league_id`, `league_name`, `userId`, `start_date`, `end_date`, `status`, `duration`, `max_teams`, `location`) VALUES
+(46, 'Leaguepro', 67, 2080, 2080, 'pending', 1, 1, 'Kathmandu'),
+(50, 'Nepal Super League', 72, 2025, 2025, 'pending', 1, 4, 'Kathmandu');
 
 -- --------------------------------------------------------
 
@@ -231,11 +249,9 @@ CREATE TABLE `player` (
 --
 
 INSERT INTO `player` (`player_id`, `p_name`, `age`, `position`, `club_id`, `phone_number`, `created_by`) VALUES
-(18, 'neymar', 34, 'lw', 49, '878677777777777', 'Diwas Shrestha'),
-(19, 'messi', 37, 'rw', 51, '8', 'Diwas Shrestha'),
-(20, 'ronaldo', 40, 'st', 52, '766766', 'Diwas Shrestha'),
-(21, 'pedri', 22, 'amf', 53, '78668', 'Diwas Shrestha'),
-(22, 'jude', 20, 'cmf', 49, '8678', 'Diwas Shrestha');
+(32, 'Bipin Kandel', 22, 'fw', 101, '98873284', 'org'),
+(33, 'Kamal Bahadur Thapa', 22, 'MF', 117, '9878767667', 'org'),
+(34, 'Joe Aidoo', 33, 'DF', 111, '987346743', 'org');
 
 -- --------------------------------------------------------
 
@@ -257,9 +273,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userId`, `name`, `email`, `password`, `created_at`, `status`) VALUES
-(61, 'org', 'org@gmail.com', '$2y$10$b3l7sT50gAxn64TGViZDBeJB0j0hIB4vDjTPcz1ddR5g/3ksJ5bnG', '2024-12-26 16:47:42', 'approved'),
-(62, 'Diwas Shrestha', 'diwas254@gmail.com', '$2y$10$9wMbzLaLbSKYeOR10gUbdeWkE6ewR3tzvaTv1HBBZR0KyLM8F0EaW', '2024-12-27 07:06:17', 'approved'),
-(63, 'deepak', 'deepak@gmail.com', '$2y$10$vmtjCd.0FXnFVZ26ANhpbuzhv6xHvGdAP4.ikf78FhGC7oR4YLYqO', '2024-12-27 08:39:27', 'approved');
+(67, 'Diwas Shrestha', 'diwas254@gmail.com', '$2y$10$/i6NlzYTQmqw.JkHkThx5eOdX4Ho3X.9ckvyQQxQyH9pUDXlgKsym', '2025-02-23 08:13:31', 'approved'),
+(72, 'org', 'org1@gmail.com', '$2y$10$mThD0ZHzz7AKO8pkH8Erf.DpKZZjCzvScsW/5u3DwLNw2pVZr6xvm', '2025-02-26 10:02:12', 'approved');
 
 --
 -- Indexes for dumped tables
@@ -340,55 +355,55 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `club`
 --
 ALTER TABLE `club`
-  MODIFY `club_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `club_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT for table `coach`
 --
 ALTER TABLE `coach`
-  MODIFY `coach_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `coach_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `form`
 --
 ALTER TABLE `form`
-  MODIFY `form_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `form_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `game`
 --
 ALTER TABLE `game`
-  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT for table `leaderboard`
 --
 ALTER TABLE `leaderboard`
-  MODIFY `leaderboard_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `leaderboard_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `league`
 --
 ALTER TABLE `league`
-  MODIFY `league_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `league_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `player`
 --
 ALTER TABLE `player`
-  MODIFY `player_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `player_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- Constraints for dumped tables
